65d34dd800ad8b4df6fa1b14f2439bc4432979cc
Bicanic-Alen <alen.bicanic01@gmail.com>
2023-10-30 00:51:43 +0000

    registra

--- emotionalsongs/src/app/app-routing.module.ts
+++ emotionalsongs/src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginRegistratiComponent } from './login-registrati/login-registrati.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { SearchComponent } from './search/search.component';
+import { RegistraComponent } from './registra/registra.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component : SearchComponent},
   { path: 'login_registrati', component : LoginRegistratiComponent},
-  { path: 'playlist', component : PlaylistComponent}
+  { path: 'playlist', component : PlaylistComponent},
+  { path: 'registra', component : RegistraComponent},
+  { path: 'login', component: LoginComponent}
 
 ];
 
--- emotionalsongs/src/app/registra/registra.component.ts
+++ emotionalsongs/src/app/registra/registra.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./registra.component.css']
 })
 export class RegistraComponent {
-
+  hide = true;
 }
